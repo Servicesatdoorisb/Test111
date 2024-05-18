801c169887f2e32a2c4e2ae743f23c43fbea222d
aa <s@local.com>
2024-05-19 03:03:15 +0500

    ok

--- javascript.js
+++ javascript.js
@@ -1,21 +1,50 @@
+var i=1;
+function executor(resolve,reject){
+    i++;
+    if(i==4)
+        {
+    resolve(i);
+        }
+else
+{
+reject(i);
+        }
+    
+}
+
+
 function getData(){
-    return new Promise( 
-
-            (a, b)=>{
-                    setTimeout(  ()=>{ b('ok');}  , 1000); 
-             }
-
-     );
-
+    return new Promise(executor);
 }
 
 getData()
-.then(
-    p=>{console.log(p);}
-
+.then(  
+    (d)=>{console.log('ressolve11 ' + d);
+        return new Promise(executor);
+    }
 )
-.catch(
-
-    q=>{console.error(q);}
-
+.catch(  
+    (d)=>{console.log('reject11 ' + d);
+        return new Promise(executor);
+    }
+)
+.then(  
+    (d)=>{console.log('ressolve22 ' + d);
+        return new Promise(executor);
+    }
+)
+.catch(  
+    (d)=>{console.log('reject22 ' + d);
+        return new Promise(executor);
+    }
+).then
+(  
+    (d)=>{console.log('ressolve33 ' + d);
+        return new Promise(executor);
+    }
+)
+.catch(  
+    (d)=>{console.log('reject33 ' + d);
+    console.log('ended ctaches');;
+    }
 )
